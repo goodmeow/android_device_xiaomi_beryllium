00813be50c7c219524546b584029425e5031f7c7
YumeMichi <do4suki@gmail.com>
2020-01-02 12:10:43 +0800

    Revert "beryllium: Enable double tap to wake feature"
    This reverts commit bc97ec0105bfb76729c9d6416b2434c734649bf6.

--- device.mk
+++ device.mk
@@ -21,8 +21,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay

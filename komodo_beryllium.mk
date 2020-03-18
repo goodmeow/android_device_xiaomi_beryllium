507a085dc6e452e114291022ad14d29d985f6c07
goodmeow <harunbam3@gmail.com>
2020-03-19 04:40:54 +0700

    komodo: update makefile

--- komodo_beryllium.mk
+++ komodo_beryllium.mk
@@ -9,9 +9,8 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common KomodoOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
-KOMODO_BUILD_TYPE := OFFICIAL
-CURRENT_BUILD_TYPE :=gapps
-TARGET_GAPPS_ARCH :=arm64
+#CURRENT_BUILD_TYPE :=gapps
+#TARGET_GAPPS_ARCH :=arm64
 $(call inherit-product, vendor/komodo/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.

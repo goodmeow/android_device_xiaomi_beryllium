7a46c67d7e163b54deda25be4309b62d8bf5c33a
goodmeow <harunbam3@gmail.com>
2020-04-03 17:27:02 +0700

    "Ancient Gang"

--- ancient_beryllium.mk
+++ ancient_beryllium.mk
@@ -5,19 +5,19 @@
 #
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
-$(call inherit-product, vendor/xiaomi/firmware/beryllium/firmware.mk)
+#$(call inherit-product, vendor/xiaomi/firmware/beryllium/firmware.mk)
 
-# Inherit some common CORVUS-OS stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_INCLUDE_WIFI_EXT := true
-DU_BUILD_TYPE := OFFICIAL
-# CURRENT_BUILD_TYPE :=gapps
-# TARGET_GAPPS_ARCH :=arm64
+ANCIENT_BUILD_TYPE := III
+CURRENT_BUILD_TYPE :=gapps
+TARGET_GAPPS_ARCH :=arm64
 #$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := du_beryllium
+PRODUCT_NAME := ancient_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1

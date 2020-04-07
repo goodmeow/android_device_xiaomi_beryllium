1dec4507e984d7ac0c7728265210b1c48b334ff6
goodmeow <harunbam3@gmail.com>
2020-04-07 22:46:11 +0700

    beryllium: update ancient dep.

--- ancient_beryllium.mk
+++ ancient_beryllium.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/xiaomi/firmware/beryllium/firmware.mk)
 $(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_INCLUDE_WIFI_EXT := true
-ANCIENT_BUILD_TYPE := V
+ANCIENT_BUILD_TYPE := PRIMEVAL
 CURRENT_BUILD_TYPE :=gapps
 TARGET_GAPPS_ARCH :=arm64
 #$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)

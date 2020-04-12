3a8ff2725a4ab1be8b9d130cd69d001f331d2041
goodmeow <harunbam3@gmail.com>
2020-04-13 05:34:51 +0700

    exui:

---
+++ aosp_beryllium.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common KomodoOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_WIFI_EXT := true
+KOMODO_BUILD_TYPE := OFFICIAL
+CURRENT_BUILD_TYPE :=gapps
+TARGET_GAPPS_ARCH :=arm64
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    org.aosp.maintainer=ʍoǝɯpooɓ
+
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.4.0.QEJMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys" \
+    PRODUCT_NAME="beryllium" \
+    TARGET_DEVICE="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

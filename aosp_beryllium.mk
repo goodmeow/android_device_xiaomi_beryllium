46a6846aff24193ac77cdc94281d0953284284bc
goodmeow <harunbam3@gmail.com>
2020-04-13 05:35:37 +0700

    beryllium: update dep.

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,10 +6,10 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common KomodoOS stuff.
+# Inherit some common aosp stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
-KOMODO_BUILD_TYPE := OFFICIAL
+CUSTOM_BUILD_TYPE := NotreDame
 CURRENT_BUILD_TYPE :=gapps
 TARGET_GAPPS_ARCH :=arm64
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
@@ -21,14 +21,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.aosp.maintainer=ʍoǝɯpooɓ
-
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.4.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys" \
-    PRODUCT_NAME="beryllium" \
-    TARGET_DEVICE="beryllium"
+    PRODUCT_NAME="beryllium" 
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

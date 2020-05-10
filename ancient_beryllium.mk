04925475f18e3ab5c89cbe90bafcdd756baa2b64
goodmeow <harunbam3@gmail.com>
2020-05-11 05:40:36 +0700

    beryllium: update product makefile

--- ancient_beryllium.mk
+++ ancient_beryllium.mk
@@ -9,12 +9,14 @@ $(call inherit-product, vendor/xiaomi/firmware/beryllium/firmware.mk)
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/ancient/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
-ANCIENT_BUILD_TYPE := PRIMEVAL
-CURRENT_BUILD_TYPE :=gapps
+
+# Ancient
+ANCIENT_OFFICIAL := true
+ANCIENT_NOGAPPS := true
+FORCE_OTA := true
 TARGET_GAPPS_ARCH :=arm64
-#$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := ancient_beryllium
@@ -24,7 +26,7 @@ PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \

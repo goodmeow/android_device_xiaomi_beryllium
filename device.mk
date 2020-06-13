d9d195c1abe6a4735a839362b4eaef0be3c87c01
goodmeow <harunbam3@gmail.com>
2020-06-13 18:50:51 +0700

    Revert "beryllium: overlay: build PixelSetup"
    This reverts commit 065809ee2d2df25bba469003633a1b7e5d270a52.

--- device.mk
+++ device.mk
@@ -25,8 +25,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay \
-    PixelSetupWizardOverlay
+    NoCutoutOverlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal

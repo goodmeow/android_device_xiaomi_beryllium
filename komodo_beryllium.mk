f92ae759f8e3fb096f9604f0c3360e2c81e2f561
goodmeow <harunbam3@gmail.com>
2020-09-08 23:43:27 +0700

    beryllium: prop: add komodo maintainer prop

--- komodo_beryllium.mk
+++ komodo_beryllium.mk
@@ -8,9 +8,10 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common komodo stuff.
 $(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
 CURRENT_BUILD_TYPE := nogapps
-KOMODO_VARIANT := BETA
+KOMODO_VARIANT ?= BETA
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := komodo_beryllium
